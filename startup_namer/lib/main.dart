41d6de756127eef42c0f5775ff4f4e24eb08f6f1
Wallace <wallacelim97@gmail.com>
2020-11-29 10:30:07 +0800

    Add saved suggestions page

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -22,12 +22,44 @@ class _RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
   final _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      // Push a new route onto the navigator stack
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
+      appBar: AppBar(title: Text('Startup Name Generator'), actions: [
+        IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+      ]),
       body: _buildSuggestions(),
     );
   }
