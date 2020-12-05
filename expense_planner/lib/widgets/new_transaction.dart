3a9f980d6cd94f3396cc224ddab9240c5306a5b6
Wallace <wallacelim97@gmail.com>
2020-12-05 17:23:54 +0800

    Add AdaptiveFlatButton

--- expense_planner/lib/widgets/new_transaction.dart
+++ expense_planner/lib/widgets/new_transaction.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:expense_planner/widgets/adaptive_flat_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -92,16 +96,7 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
-                    FlatButton(
-                      textColor: Theme.of(context).primaryColor,
-                      child: Text(
-                        'Choose Date',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      onPressed: _presentDatePicker,
-                    ),
+                    AdaptiveFlatButton('Choose Date', _presentDatePicker)
                   ],
                 ),
               ),
