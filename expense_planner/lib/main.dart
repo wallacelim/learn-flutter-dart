16bf7c192c83bdd0de0c7230012359522240db86
Wallace <wallacelim97@gmail.com>
2020-12-08 00:06:37 +0800

    Add lifecycle methods and log statements

--- expense_planner/lib/main.dart
+++ expense_planner/lib/main.dart
@@ -49,10 +49,28 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   final List<Transaction> _userTransactions = [];
 
   bool _showChart = false;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this); // Mount an Observer
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print(state);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance
+        .removeObserver(this); // Unmount the Observer mounted in initState()
+    super.dispose();
+  }
 
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
